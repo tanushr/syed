6feecdbbc9472d81767522df6b8b1a60333f1b70
syedaffu123 <43022937+syedaffu123@users.noreply.github.com>
2018-09-20 09:02:52 +0530

    Add files via upload

---
+++ percent.py
@@ -0,0 +1,13 @@
+x=int(input("enter your marks obtained"))
+if (x>=90):
+	print("Grade A+")
+elif (x>=80):
+	print("Grade A")
+elif (x>=70):
+	print("Grade B+")
+elif (x>=60):
+	print("Grade C+")
+elif (x>=50):
+	print("Grade C")
+else:
+	print("fail")
