1260cc9d12d1ea20e23bac34ee60e77680b6f0e6
syedaffu123 <43022937+syedaffu123@users.noreply.github.com>
2018-10-11 10:29:33 +0530

    Add files via upload

---
+++ ls.py
@@ -0,0 +1,10 @@
+a=int(input("enter value of a "))
+b=int(input("enter the value of b"))
+#commutatuve law
+c=a+b
+q=b+a
+print('c','=',a,'+',b)
+print('q','=',b,'+',a)
+if(c==q):
+	print('c=q','it satisfy commutative law')
+	
