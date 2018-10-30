5d7fc33e5bce5e360d32518e362e2a90fa7d008e
syedaffu123 <43022937+syedaffu123@users.noreply.github.com>
2018-10-30 11:46:21 +0530

    Add files via upload

---
+++ graph2.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+#x coordinates of left sides of bars
+left = [1,2,3,4,5,]
+#heights of bar
+height = [10,24,36,40,5]
+#labels for bars
+tick_label = ['one','two','three','four','five']
+#plotting a bar chart
+plt.bar(left,height,tick_label = tick_label,
+		width = 0.8, color = ['red', 'green'])
+#x-axis
+plt.xlabel('x-axis')
+#y-axis
+plt.ylabel('y-axis')
+#title
+plt.title("my second graph")
+plt.show()
