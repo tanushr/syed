ad23c8fe4cdf234e95fee9f442328808486ae024
syedaffu123 <43022937+syedaffu123@users.noreply.github.com>
2018-11-13 12:43:23 +0530

    Update sr.py

--- sr.py
+++ sr.py
@@ -2,9 +2,9 @@ import speech_recognition as sr
 AUDIO_FILE = ("example.wav")
 r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source :
-audio = r.record(source)
+	audio = r.record(source)
 try:
-	print("The audio file contains: + r.recognize_google(audio)")
+	print("The audio file contains:" + r.recognize_google(audio))
 except sr.UnknownValueError:
 	print("Google speech recognition could not ")
 except sr.RequestError as e:
